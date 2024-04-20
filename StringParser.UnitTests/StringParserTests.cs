37670d091d1cda8e7e6889afc514b285bbadc3fc
E-K8 <ekatplat@gmail.com>
2024-04-20 20:54:12 +0100

    test replacing $ signs with £ signs

--- StringParser.UnitTests/StringParserTests.cs
+++ StringParser.UnitTests/StringParserTests.cs
@@ -42,6 +42,23 @@ public class StringParserTests
         // Assert
         result.ShouldBe(expected, "Contiguous duplicate characters should be reduced to a single character in the same case.");
     }
+ 
+    [TestCase("mid: e$sential", "mid: e£sential")]
+    [TestCase("end: pqr$", "end: pqr£")]
+    [TestCase("start: $tart", "start: £tart")] // fails  
+    [TestCase("$$$$$ £££ ££ $$", "£ £ £ £")]
+    [TestCase("No change here", "No change here")]
+    [TestCase("$$$ becomes £££", "£ becomes £")] 
+    [TestCase("$1 is < than £2", "£1 is < than £2")] // fails
+    public void Parse_ShouldReplaceDollarSignsWithPoundSigns(string input, string expected)
+    {
+        // Arrange
+        var stringParser = new StringProcessor();
 
-   
+        // Act
+        var result = stringParser.Parse(input);
+
+        // Assert
+        result.ShouldBe(expected, $"Input '{input ?? "null"}' should be transformed to '{expected}'.");
+    }
 }
