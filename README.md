bb3287d28bae13e64c3ab2dbb156a2408b1b03dd
E-K8 <ekatplat@gmail.com>
2024-04-21 16:29:35 +0100

    test that collections are not null (fail), update README

--- README.md
+++ README.md
@@ -1,13 +1,40 @@
 # QA Automation Tester
 
+## Notes:
+
+My tests check the following:
+
+- the output string is truncated to 15 chars
+- the duplicate characters are reduced to 1 of the same case
+- $ signs are replaced with £ signs
+- underscores and 4s are removed
+- output is never null or empty
+- the output collection is never null
+
+## Findings
+
+The following tests fail:
+
+- output is never null or empty
+- the output collection is never null
+
+## Possible solutions
+
+For "output is never null or empty":
+
+- One approach could be modify the "Parse" method to return a default string, such as 'default', when faced with null or empty inputs. Alternatively, we could consider a placeholder string that indicates the input was invalid.
+
+For "the output collection is never null":
+
+- `Parse` method throws `NullReferenceException` with null input. A potential solution could involve modifying the `Parse` method to check for null inputs and return an empty collection in these cases.
+
 ## The Brief
 
 A developer was given the following task:
 
-
-> Write C# code to process a collection of string values which are passed to a method which returns a collection of processed strings. The input strings may be any length and can contain any character. Output strings must not be null or empty string, should be truncated to max length of 15 chars, and contiguous duplicate characters in the same case should be reduced to a single character in the same case. Dollar sign ($) should be replaced with a pound sign (£), and underscores (_) and number 4 should be removed. Code should be test-driven, efficient, re-usable and loosely coupled. The returned collection must not be null.
+> Write C# code to process a collection of string values which are passed to a method which returns a collection of processed strings. The input strings may be any length and can contain any character. Output strings must not be null or empty string, should be truncated to max length of 15 chars, and contiguous duplicate characters in the same case should be reduced to a single character in the same case. Dollar sign ($) should be replaced with a pound sign (£), and underscores (\_) and number 4 should be removed. Code should be test-driven, efficient, re-usable and loosely coupled. The returned collection must not be null.
 >
-> Example input string:  AAAc91%cWwWkLq$1ci3_848v3d__K
+> Example input string: AAAc91%cWwWkLq$1ci3_848v3d\_\_K
 
 Your task is to write a set of tests around the developers work to prove it has been suitably done to the given specifications.
 
@@ -15,7 +42,8 @@ Your task is to write a set of tests around the developers work to prove it has 
 
 The solution is a set of C# library projects. These projects contain the developers implementation and a placeholder project for unit tests.
 
-The unit test project  has the following NuGet packages already installed to facilitate writing tests:
+The unit test project has the following NuGet packages already installed to facilitate writing tests:
+
 - NUnit
 - Moq
 - Shouldly
--- StringParser.UnitTests/StringParserTests.cs
+++ StringParser.UnitTests/StringParserTests.cs
@@ -7,29 +7,27 @@ namespace StringParser.UnitTests;
 [TestFixture]
 public class StringParserTests
 {
-    [Test]
-    public void Test()
-    {
-        1.ShouldBe(1);
-    }
-
-      [Test]
-    public void StringIsTruncatedTo15Chars_WhenLongerThan15Chars()
+    // Test cases to ensure the output string is truncated to 15 chars
+    [TestCase("12345678901234567890", "123567890123567")]
+    [TestCase("Short", "Short")]
+    [TestCase("Exactly15qwerty", "Exactly15qwerty")]
+    [TestCase("long Write C# code to process a collection of string values which are passed to a method which returns a collection of processed strings.", "long Write C# c")] 
+    
+    public void ShouldTruncateTo15Chars(string input, string expected)
     {
         // Arrange
-        var stringParser = new StringProcessor();
-        var input = "WhetherTheWeatherIsGood"; 
-        var expected = "WhetherTheWeath"; 
+        var stringParser = new StringProcessor(); 
 
         // Act
         var result = stringParser.Parse(input);
 
         // Assert
-        result.ShouldBe(expected);
+        result.ShouldBe(expected, $"Input '{input}' should be truncated to '{expected}'.");
     }
 
+      // Test case to ensure the duplicate characters are reduced
       [Test]
-    public void Parse_ReduceContiguousDuplicateCharacters()
+    public void ReduceContiguousDuplicateCharacters()
     {
         // Arrange
         var stringParser = new StringProcessor(); 
@@ -42,7 +40,8 @@ public class StringParserTests
         // Assert
         result.ShouldBe(expected, "Contiguous duplicate characters should be reduced to a single character in the same case.");
     }
- 
+
+    // Test cases to ensure the replacement of $ sign with £ sign
     [TestCase("mid: e$sential", "mid: e£sential")]
     [TestCase("end: pqr$", "end: pqr£")]
     [TestCase("start: $tart", "start: £tart")]   
@@ -50,7 +49,7 @@ public class StringParserTests
     [TestCase("No change here", "No change here")]
     [TestCase("$$$ becomes £££", "£ becomes £")] 
     [TestCase("$1 is < than £2", "£1 is < than £2")] 
-    public void Parse_ShouldReplaceDollarSignsWithPoundSigns(string input, string expected)
+    public void ShouldReplaceDollarSignsWithPoundSigns(string input, string expected)
     {
         // Arrange
         var stringParser = new StringProcessor();
@@ -61,15 +60,13 @@ public class StringParserTests
         // Assert
         result.ShouldBe(expected, $"Input '{input ?? "null"}' should be transformed to '{expected}'.");
     }
- 
+
+    // Test cases to ensure the removal of underscores and number 4
     [TestCase("un_der_sco_res", "underscores")]
     [TestCase("one4andtwo4and", "oneandtwoand")]
     [TestCase("Com4bi_ne", "Combine")]
-    [TestCase("____4___4___", "")] 
-    [TestCase("4444", "")] 
-    [TestCase("____", "")]
 
-    public void Parse_ShouldRemoveUnderscoresAndNumber4(string input, string expected)
+    public void ShouldRemoveUnderscoresAndNumber4(string input, string expected)
     {
         // Arrange
         var stringParser = new StringProcessor(); 
@@ -86,7 +83,7 @@ public class StringParserTests
     [TestCase("4444")] 
     [TestCase("")]
     [TestCase(null)]    
-    public void Parse_ShouldNotReturnNullOrEmptyStrings(string input)
+    public void ShouldNotReturnNullOrEmptyStrings(string input)
     {
         // Arrange
         var stringParser = new StringProcessor(); 
@@ -96,5 +93,30 @@ public class StringParserTests
 
         // Assert
         result.ShouldNotBeNullOrEmpty();
+    }
+
+    // Test case to ensure the output collection is never null
+     [Test]
+    public void CollectionShouldNotBeNull()
+    {
+        // Arrange
+        var stringCollectionParser = new StringCollectionParser(new StringProcessor()); 
+        
+        var testCases = new List<IEnumerable<string>>
+        {
+            new List<string> { "test", "another test" }, 
+            new List<string>(), 
+            new List<string> { null, "" }, 
+            null 
+        };
+
+        foreach (var testCase in testCases)
+        {
+            // Act
+            var result = stringCollectionParser.Parse(testCase);
+
+            // Assert
+            result.ShouldNotBeNull("The returned collection should not be null regardless of input.");
+        }
     }
 }
